100b78ca86825ad8d6d606ee2c8f4661e1a39f81
Samsam <ec21322@qmul.ac.uk>
2023-11-16 19:32:55 +0000

    profile_image and dob to user, installed pillow

--- api/models.py
+++ api/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import date
 
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(max_length=254, unique=True)
+    profile_image = models.ImageField(upload_to="api\static\api\spa\assets\profile_images", null=True, blank=True)
+    dob = models.DateField(default=date.today)
